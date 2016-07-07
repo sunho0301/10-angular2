ee94d091019bdb1048b511dff9d3e326cc7bfdf6
sunho0301 <sunho4572@gmail.com>
2016-07-06 19:22:00 -0700

    hmm

--- js/movies.js
+++ js/movies.js
@@ -1,1 +1,57 @@
 'use strict';
+
+
+var myApp = angular.module('MoviesApp', []);
+
+myApp.controller('MoviesCtrl',['$scope', '$http', function($scope, $http){
+    $scope.message = 'Hello World'; // a model for testing
+
+    $http.get('data/movies-2015.json').then(function(response) {
+        var data = response.data;
+        $scope.movies = data;
+    });
+    
+    // not perfectly working. fix code.
+    $scope.ordering = '-gross'
+    $scope.sortBy = function(order) {
+        if($scope.ordering == order) {
+            $scope.orderging = -order;
+        } else {
+            $scope.ordering = order;
+        }
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    // sample data // one model
+    $scope.movies = [
+     { 
+       title: "An Example Movie",
+       released: "7/6/16",
+       distributor: "INFO 343",
+       genre: "Educational",
+       rating: "PG",
+       gross: 1000000,
+       tickets: 30
+     },
+     { title: "Another Movie", released: "1/1/15", distributor: "Universal", genre: "Action", rating: "PG-13", gross: 1234567, tickets: 8901237 },
+     { title: "Star Wars Ep. VII: The Force Awakens", released: "12/18/15", distributor: "Walt Disney", genre: "Adventure", rating: "PG-13", gross: 742208942, tickets: 88043765 }
+    ];
+    */
+
+
+
+}]);
--- js/payment.js
+++ js/payment.js
@@ -1,11 +1,16 @@
 'use strict';
 
-var app = angular.module('PaymentApp', []);
+var app = angular.module('PaymentApp', ['credit-cards']);
 app.controller('FormCtrl', ['$scope', function($scope) { 
 
 
 	//a function to call when submitted
   $scope.submitForm = function(form){
+		console.log('$valid', form.username.$valid);
+		console.log('$error', form.username.$error);
+
+
+
   	if(form.$valid) {
   		window.alert('Payment info has been submitted!');
   	}
